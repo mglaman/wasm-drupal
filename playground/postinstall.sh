0cd76e6bec360e8076a5f2ebb31ff23d2a25c6fd
Matt Glaman <matt.glaman@acquia.com>
2024-06-28 15:03:38 -0500

    Fix php-wasm version numbers; simplify postinstall

--- playground/postinstall.sh
+++ playground/postinstall.sh
@@ -1,4 +1,2 @@
-# This could be `node_modules/php-*/*.mjs*` after https://github.com/seanmorris/php-wasm/issues/45 is fixed.
-rsync --exclude='*[nN]ode*' --exclude='*[wW]ebview*' --exclude='php-tags*' node_modules/php-wasm/*.mjs* public
-rsync --exclude='*[nN]ode*' --exclude='*[wW]ebview*' --exclude='php-tags*' node_modules/php-cgi-wasm/*.mjs* public
+rsync --exclude='*[nN]ode*' --exclude='*[wW]ebview*' --exclude='php-tags*' --exclude='*index*' node_modules/php-*/*.mjs* public
 rsync --exclude='php8.0*' --exclude='php8.1*' --exclude='php8.2*' node_modules/*/*.so public

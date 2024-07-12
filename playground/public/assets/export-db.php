d162d5b7baa51da99d1b5f09aa1b9d149f9ec543
Matt Glaman <nmd.matt@gmail.com>
2024-07-12 10:41:41 -0500

    add indexes, set sequences

--- playground/public/assets/export-db.php
+++ playground/public/assets/export-db.php
@@ -19,34 +19,24 @@ $kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod');
 chdir($kernel->getAppRoot());
 $kernel->boot();
 
-$sql = '';
+$sql = 'PRAGMA foreign_keys=OFF;' . PHP_EOL;
+$sql .= 'BEGIN TRANSACTION;' . PHP_EOL;
 
 // adapted from https://github.com/ephestione/php-sqlite-dump/blob/master/sqlite_dump.php
 $database = \Drupal::database();
-$tables = $database->query('SELECT [name] FROM {sqlite_master} WHERE [type] = "table" AND [name] NOT LIKE "sqlite_%" ORDER BY name');
+$tables = $database->query('SELECT [name], [sql] FROM {sqlite_master} WHERE [type] = "table" AND [name] NOT LIKE "sqlite_%" ORDER BY name');
 foreach ($tables as $table) {
-    $sql .= '--' . PHP_EOL;
-    $sql .= "-- Table structure for table `{$table->name}`" . PHP_EOL;
-    $sql .= '--' . PHP_EOL;
-    $sql .= $database->query('SELECT [sql] FROM {sqlite_master} WHERE [name] = :name ', [
-        ':name' => $table->name,
-    ])->fetchField();
-    $sql .= PHP_EOL.PHP_EOL;
+    $sql .= $table->sql . PHP_EOL;
 
     $columns = array_map(
         fn (object $row) => "`{$row->name}`",
         $database->query("PRAGMA table_info({$table->name})")->fetchAll()
     );
     $columns = implode(', ', $columns);
-
-    $sql .= '--' . PHP_EOL;
-    $sql .= "-- Dumping data for table `{$table->name}`" . PHP_EOL;
-    $sql .= '--' . PHP_EOL;
     $sql .= PHP_EOL;
 
-    $sql .= "INSERT INTO {$table->name} ($columns) VALUES" . PHP_EOL;
-
     $rows = $database->select($table->name)->fields($table->name)->execute()->fetchAll(\PDO::FETCH_ASSOC);
+    $sql .= "INSERT INTO {$table->name} ($columns) VALUES" . PHP_EOL;
     if (count($rows) > 0) {
         $last_row = implode(', ', array_pop($rows));
         foreach ($rows as $row) {
@@ -56,7 +46,20 @@ foreach ($tables as $table) {
         $sql .= "($last_row);";
     }
 
-    $sql .= PHP_EOL.PHP_EOL;
+    $sql .= PHP_EOL;
 }
+
+$sql .= 'DELETE FROM sqlite_sequence;' . PHP_EOL;
+$sequences = $database->query('SELECT [name], [seq] FROM sqlite_sequence');
+foreach ($sequences as $sequence) {
+    $sql .= "INSERT INTO sqlite_sequence VALUES('{$sequence->name}',{$sequence->seq});" . PHP_EOL;
+}
+
+$indexes = $database->query('SELECT [sql] FROM {sqlite_master} WHERE [type] = "index" AND [name] NOT LIKE "sqlite_%" ORDER BY name');
+foreach ($indexes as $index) {
+    $sql .= $index->sql . PHP_EOL;
+}
+
+$sql .= 'COMMIT;' . PHP_EOL;
 
 file_put_contents('/persist/dump.sql', $sql);

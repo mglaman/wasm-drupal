f3f17a1ea7476fa1f5c2e3b46e72ad5fc9658b9b
Matt Glaman <nmd.matt@gmail.com>
2024-08-23 00:31:49 -0500

    checkpoint of debugging

--- tests/install-site-phpcode.test.js
+++ tests/install-site-phpcode.test.js
@@ -54,13 +54,14 @@ describe('install-site.phpcode', () => {
         expect(loginOutput).toHaveProperty('params')
         expect(loginOutput.params).toHaveProperty('name')
         expect(loginOutput.params).toHaveProperty('id')
+        console.log(loginOutput.params.sessions)
         assertOutput(stdErr, '')
         stdOut.length = 0;
 
         const [cgiOut, cgiErr, phpCgi] = createCgiPhp({ configFixturePath, persistFixturePath });
-
         phpCgi.cookies.set(loginOutput.params.name, loginOutput.params.id)
 
+        // @todo ping a debug script to check on HTTP_COOKIES, user isn't logged in.
         const response = await phpCgi.request({
             connection: {
                 encrypted: false,
@@ -74,12 +75,11 @@ describe('install-site.phpcode', () => {
         assertOutput(cgiOut, 'GET /cgi/drupal 200')
         assertOutput(cgiErr, '')
 
-        console.log(response.headers);
-
         expect(response.headers.get('x-generator')).toMatch(/Drupal \d+ \(https:\/\/www\.drupal\.org\)/)
         const text = await response.text()
 
         console.log(text)
+
         // Assert custom site title.
         expect(text).toContain('<title>Welcome! | test</title>')
         // Verify CSS/JS aggregation turned off
@@ -104,8 +104,16 @@ describe('install-site.phpcode', () => {
         expect(stdOut.shift().trim()).toStrictEqual('{"message":"Beginning install tasks","type":"install"}')
         expect(stdOut.pop().trim()).toStrictEqual('{"message":"Performing install task (12 \\/ 12)","type":"install"}')
         assertOutput(stdErr, '')
+        stdOut.length = 0
+
+        await runPhpCode(php, rootPath + '/public/assets/login-admin.phpcode')
+        const loginOutput = JSON.parse(stdOut.join('').trim());
+        console.log(loginOutput)
 
         const [cgiOut, cgiErr, phpCgi] = createCgiPhp({ configFixturePath, persistFixturePath });
+        phpCgi.cookies.set(loginOutput.params.name, loginOutput.params.id)
+
+        // @todo ping a debug script to check on HTTP_COOKIES, user isn't logged in.
         await phpCgi.request({
             connection: {
                 encrypted: false,
@@ -120,5 +128,5 @@ describe('install-site.phpcode', () => {
         assertOutput(cgiErr, '')
     })
 }, {
-    timeout: 90000
+    timeout: 999999
 })
